f08ae7ef647bd543073d493cdd0d615c9c653067
stefankrasnow <jocrednow@gmail.com>
2024-08-12 12:55:28 +0200

    init project

---
+++ TourMap/Models/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  TourMap
+//
+//  Created by Stepan Krasnov on 12/08/2024.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
