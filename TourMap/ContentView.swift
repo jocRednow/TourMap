ab14bdf46d11ca79b20c7976272e658cf30685b1
stefankrasnow <jocrednow@gmail.com>
2024-08-12 12:30:01 +0200

    Initial Commit

---
+++ TourMap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TourMap
+//
+//  Created by Stepan Krasnov on 12/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TourMap/TourMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  TourMapApp.swift
+//  TourMap
+//
+//  Created by Stepan Krasnov on 12/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct TourMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
