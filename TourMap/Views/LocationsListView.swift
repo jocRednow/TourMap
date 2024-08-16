3f9b9ab17b0a07699bab80fc5d8cb50978dd3705
stefankrasnow <jocrednow@gmail.com>
2024-08-16 14:52:07 +0200

    locations list view

---
+++ TourMap/Views/LocationsListView.swift
@@ -0,0 +1,56 @@
+//
+//  LocationsListView.swift
+//  TourMap
+//
+//  Created by Stepan Krasnov on 16/08/2024.
+//
+
+import SwiftUI
+
+struct LocationsListView: View {
+    
+    @EnvironmentObject private var vm: LocationsViewModel
+    
+    var body: some View {
+        List {
+            ForEach(vm.locations) { location in
+                Button {
+                    vm.showNextLocation(location: location)
+                } label: {
+                    listRowView(location: location)
+                }
+                .padding(.vertical, 4)
+                .listRowBackground(Color.clear)
+            }
+        }
+        .listStyle(PlainListStyle())
+    }
+}
+
+#Preview {
+    LocationsListView().environmentObject(LocationsViewModel())
+}
+
+extension LocationsListView {
+    
+    private func listRowView(location: Location) -> some View {
+        HStack {
+            if let imageName = location.imageNames.first {
+                Image(imageName)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 45, height: 45)
+                    .cornerRadius(10)
+            }
+            
+            VStack(alignment: .leading) {
+                Text(location.name)
+                    .font(.headline)
+                Text(location.cityName)
+                    .font(.subheadline)
+            }
+            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+        }
+    }
+    
+}
--- TourMap/Views/LocationsView.swift
+++ TourMap/Views/LocationsView.swift
@@ -17,6 +17,13 @@ struct LocationsView: View {
         ZStack {
             Map(coordinateRegion: $vm.mapRegion)
                 .ignoresSafeArea()
+            
+            VStack(spacing: 0) {
+                header
+                    .padding()
+                
+                Spacer()
+            }
         }
     }
 }
@@ -24,4 +31,40 @@ struct LocationsView: View {
 #Preview {
     LocationsView()
         .environmentObject(LocationsViewModel())
+}
+
+extension LocationsView {
+    
+    private var header: some View {
+        VStack {
+            Button(action: {
+                vm.toggleLocationsList()
+            }, label: {
+                Text(vm.mapLocation.name + ", " + vm.mapLocation.cityName)
+                    .font(.title2)
+                    .fontWeight(.black)
+                    .foregroundColor(.primary)
+                    .frame(height: 55)
+                    .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                    .animation(.none, value: vm.mapLocation)
+                    .overlay(alignment: .leading) {
+                        Image(systemName: "arrow.down")
+                            .font(.headline)
+                            .foregroundColor(.primary)
+                            .padding()
+                            .rotationEffect(
+                                Angle(degrees: vm.showLocationsList ? 540 : 0)
+                            )
+                    }
+            })
+            
+            if vm.showLocationsList {
+                LocationsListView()
+            }
+        }
+        .background(.thinMaterial)
+        .cornerRadius(10)
+        .shadow(color: Color.black.opacity(0.3), radius: 20, x: 0, y: 15)
+    }
+    
 }
